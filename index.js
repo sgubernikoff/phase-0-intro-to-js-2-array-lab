e730c43a6123057a5f03a4b6aa6f3c5ec4b88859
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-02-02 13:30:30 -0500

    commit tree d2d3c882a3fba61d272d3f0788c1d71fad8790e3

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-const cats = ["Milo", "Otis", "Garfield"];
-function destructivelyAppendCat(name) {
+// const cats = ["Milo", "Otis", "Garfield"];
+// function destructivelyAppendCat(name) {
+//   cats.push("Ralph");
+// }
+// function destructivelyPrependCat(name) {
+//   cats.unshift("Bob");
+// }
+// function destructivelyRemoveLastCat() {
+//   cats.pop();
+// }
+// function destructivelyRemoveFirstCat() {
+//   cats.shift();
+// }
+
+// function appendCat(name) {
+//   const newCats = [...cats, "Broom"];
+//   return newCats;
+// }
+
+// function prependCat(name) {
+//   const newerCats = ["Arnold", ...cats];
+//   return newerCats;
+// }
+
+// function removeLastCat() {
+//   const freshCats = cats.slice(0, 2);
+//   return freshCats;
+// }
+// function removeFirstCat() {
+//   const fresherCats = cats.slice(1);
+//   return fresherCats;
+// }
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
   cats.push("Ralph");
 }
-function destructivelyPrependCat(name) {
+
+function destructivelyPrependCat() {
   cats.unshift("Bob");
 }
+
 function destructivelyRemoveLastCat() {
   cats.pop();
 }
@@ -12,21 +46,24 @@ function destructivelyRemoveFirstCat() {
   cats.shift();
 }
 
-function appendCat(name) {
-  const newCats = [...cats, "Broom"];
-  return newCats;
+function appendCat() {
+  let newArray = [...cats, "Broom"];
+  return newArray;
 }
 
-function prependCat(name) {
-  const newerCats = ["Arnold", ...cats];
+function prependCat() {
+  let newerCats = ["Arnold", ...cats];
   return newerCats;
 }
 
 function removeLastCat() {
-  const freshCats = cats.slice(0, 2);
-  return freshCats;
+  let newyCats = [...cats];
+  newyCats.pop();
+  return newyCats;
 }
+
 function removeFirstCat() {
-  const fresherCats = cats.slice(1);
-  return fresherCats;
+  let newestCats = [...cats];
+  newestCats.shift();
+  return newestCats;
 }
